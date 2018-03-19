325c62f7cbad85c630eddeec3190b72222b3c6d0
Miriam Armada <miriam_armada@yahoo.com>
2018-03-19 15:34:07 -0700

    update final

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -27,43 +27,24 @@ $("#addTrain").on("click", function (event) {
   frequency = $("#frequency").val().trim();
   timeA = $("#firstArr").val().trim();
 
+  $("#trainName").empty();
+  $("#destination").empty();
+  $("#frequency").empty();
+  $("#firstArr").empty();
+
+
   console.log(trainName);
   console.log(destination);
   console.log(frequency);
   console.log(timeA);
 
 
-  // First Time (pushed back 1 year to make sure it comes before current time)
-  var firtArrConv = moment(timeA, "HH:mm").subtract(1, "years");
-  console.log(firtArrConv);
-
-  // Current Time
-  var currentTime = moment();
-  console.log(moment(currentTime).format("hh:mm"));
-
-  // Difference between the times
-  var diffTime = moment().diff(moment(firtArrConv), "minutes");
-  console.log(diffTime);
-
-  // Time apart (remainder)
-  var tRemainder = diffTime % frequency;
-  console.log(tRemainder);
-
-  // Minute Until Train
-  var tMinutesTillTrain = frequency - tRemainder;
-  console.log(tMinutesTillTrain);
-
-  // Next Train
-  nextTrain = moment().add(tMinutesTillTrain, "minutes");
-  console.log(moment(nextTrain).format("hh:mm"));
-
 
   var newTrain = {
     train: trainName,
     destination: destination,
     frequency: frequency,
-    nextTrain: nextTrain.toLocaleString(),
-    minA: tMinutesTillTrain,
+    firstTrain: timeA,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
 
   };
@@ -73,30 +54,121 @@ $("#addTrain").on("click", function (event) {
 
   database.ref().push(newTrain);
 
-  database.ref().orderByChild("dateAdded").limitToLast(15).on("child_added", function(snapshot) {
-      // storing the snapshot.val() in a variable for convenience
-      var sv = snapshot.newTrain.val();
+});
 
-      // Console.loging the last user's data
-      console.log(sv.train);
-      console.log(sv.destination);
-      console.log(sv.frequency);
-      console.log(moment(sv.nextTrain).format("hh:mm"));
-      console.log(sv.tMinutesTillTrain);
+  database.ref().orderByChild("dateAdded").limitToLast(15).on("child_added", function (snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
 
-      $("#trainSch").append("<tr><td>" + sv.train + 
-	      	"</td><td>" + sv.destination + 
-	      	"</td><td>" + sv.frequency + 
-          "</td><td>" + moment(sv.nextTrain).format("hh:mm") + 	
-          "</td><td>" + sv.tMinutesTillTrain +("</td></tr>")
-		); 
+    // Console.loging the last user's data
+    console.log(sv.train);
+    console.log(sv.destination);
+    console.log(sv.frequency);
+    console.log(moment(sv.firstTrain).format("hh:mm"));
 
-  
 
-      // Handle the errors
-    }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firtArrConv = moment(sv.firstTrain, "HH:mm").subtract(1, "years");
+    console.log(firtArrConv);
+
+    // Current Time
+    var currentTime = moment();
+    console.log(moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firtArrConv), "minutes");
+    console.log(diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % sv.frequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = sv.frequency - tRemainder;
+    console.log(tMinutesTillTrain);
+
+    // Next Train
+    nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log(moment(nextTrain).format("hh:mm"));
+
+    var train = sv.train.toUpperCase();
+    var dest = sv.destination.toUpperCase();
+
+
+    $("#trainSch").append("<tr><td>" + train +
+      "</td><td>" + dest +
+      "</td><td>" + sv.frequency +
+      "</td><td>" + moment(nextTrain).format("hh:mm A") +
+      "</td><td>" + tMinutesTillTrain + ("</td></tr>")
+    );
+
+
+
+    // Handle the errors
+  }, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+
+
+
+
+//Load Existing Trains when page loads
+
+$(document).ready(function () {
+
+  database.ref().orderByChild("dateAdded").limitToLast(15).on("value", function (snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.loging the last user's data
+    console.log(sv.train);
+    console.log(sv.destination);
+    console.log(sv.frequency);
+    console.log(moment(sv.firstTrain).format("hh:mm"));
+
+
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firtArrConv = moment(sv.firstTrain, "HH:mm").subtract(1, "years");
+    console.log(firtArrConv);
+
+    // Current Time
+    var currentTime = moment();
+    console.log(moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firtArrConv), "minutes");
+    console.log(diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % sv.frequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = sv.frequency - tRemainder;
+    console.log(tMinutesTillTrain);
+
+    // Next Train
+    nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log(moment(nextTrain).format("hh:mm"));
+
+
+
+
+
+    $("#trainSch").append("<tr><td>" + sv.train.toUpperCase() +
+      "</td><td>" + sv.destination.toUpperCase() +
+      "</td><td>" + sv.frequency +
+      "</td><td>" + moment(nextTrain).format("hh:mm A") +
+      "</td><td>" + tMinutesTillTrain + ("</td></tr>")
+    );
+
+    // Handle the errors
+  }, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
 
 
 });
